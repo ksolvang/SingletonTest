97c7916ec7bb0effe4b318be27ace3b8ee150da4
Kristian Solvang <kristian.solvang@capgemini.com>
2020-01-08 17:08:10 +0100

    Add project files.

---
+++ SingletonTest/LongRunningTasks.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonTest
+{
+    internal sealed class LongRunningTasks
+    {
+        public async Task<string> FiveSecondsAsync()
+        {
+            await Task.Delay(5000);
+            return "blah";
+        }
+    }
+}
---
+++ SingletonTest/Singleton.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonTest
+{
+    internal sealed class Singleton
+    {
+        private Singleton()
+        {
+        }
+
+        private static readonly Lazy<Singleton>
+        lazy = new Lazy<Singleton> (() => new Singleton());
+
+        public static Singleton Instance { get { return lazy.Value; } }
+
+        //private static Singleton instance = null;
+        //private static readonly object padlock = new object();
+
+        //Singleton()
+        //{
+        //}
+
+        //public static Singleton Instance
+        //{
+        //    get
+        //    {
+        //        lock (padlock)
+        //        {
+        //            if (instance == null)
+        //            {
+        //                instance = new Singleton();
+        //            }
+        //            return instance;
+        //        }
+        //    }
+        //}
+    }
+}
